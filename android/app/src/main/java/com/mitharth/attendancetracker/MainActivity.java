017fcd7fa23e9d3f83005480363dfd0386b7f582
Mitharth Jain <mitharth@Mitharths-MacBook.local>
2026-08-29 13:01:00 +0530

    Add GitHub Actions workflow for Windows and Mac build

---
+++ android/app/src/main/java/com/mitharth/attendancetracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mitharth.attendancetracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mitharth.attendancetracker',
+  appName: 'Attendance Tracker',
+  webDir: 'dist'
+};
+
+export default config;
